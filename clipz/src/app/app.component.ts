c29ce278dbd1cccc4652941f36e4f46688acc9a6
TheChiran <tonmoychiran449@gmail.com>
2022-11-06 21:51:52 +0600

    [Task-> 1. Included clipz project]

---
+++ clipz/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { ModalService } from 'src/app/services/modal.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+
+}
