01e922ea958a4ddf229659144a93be8cf691176c
TheChiran <tonmoychiran449@gmail.com>
2022-11-21 20:19:24 +0600

    [Task-> 1. Made ready for vercel deployment]

--- clipz/src/environments/environment.prod.ts
+++ clipz/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDsHYNkuJu-SyHgJBI9QMJwKgk7jcmkG80',
+    authDomain: 'clips-4b46d.firebaseapp.com',
+    projectId: 'clips-4b46d',
+    storageBucket: 'clips-4b46d.appspot.com',
+    appId: '1:852690150298:web:ce6f17f0e4d90b62219023',
+  },
 };
